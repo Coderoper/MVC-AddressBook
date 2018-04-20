171c9e066aa1affe3cd3d6ce1590105551f83e6c
Austin Roberts <d.austin.roberts@gmail.com>
2018-04-20 15:48:34 -0700

    This takes user inputs and adds names with a hyper link to phone and address info on another page

---
+++ Controller/HomeController.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using AddressBook.Models;
+
+namespace AddressBook.Controllers
+{
+    //controller class
+    public class HomeController : Controller
+    {
+        //Get and post routes
+        //this will be the homepage
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+          //define list as allcontacts
+          List<Contact> allContacts = Contact.GetAll();
+          //passes allContacts to view and looks for Index.cshtml under view folder
+          return View(allContacts);
+        }
+        //this is where you are routed when you click add new contact
+        [HttpGet("/contacts/new")]
+        public ActionResult CreateForm()
+        {
+          //when you submit the form you are returned to the homepage with View()
+          return View();
+        }
+        [HttpPost("/contacts")]
+        //creates a new contact and adds to list allcontacts
+        public ActionResult Create()
+        {
+          //takes user input to create newContact
+          Contact newContact = new Contact(Request.Form["new-name"], Request.Form["new-phone"], Request.Form["new-address"]);
+          List<Contact> allContacts = Contact.GetAll();
+          return View("Index", allContacts);
+        }
+        [HttpGet("/contacts/{id}")]
+        public ActionResult Details(int id)
+        {
+            Contact contact = Contact.Find(id);
+            return View(contact);
+        }
+        // [HttpPost("/contacts/clearall")]
+        // public ActionResult Cleared()
+        // {
+        //   Contact.ClearAll();
+        //   return RedirectToRoute("Index");
+        //
+        // }
+    }
+}
---
+++ Models/AddressBook.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace AddressBook.Models
+{
+  //main class
+  public class Contact
+  {
+    private string _name;
+    private string _phone;
+    private string _address;
+    private int _id;
+    private static List<Contact>_contacts = new List<Contact> {};
+
+    //constructor
+    public Contact (string name, string phone, string address)
+    {
+      //this is where I add contacts to the list
+      _contacts.Add(this);
+      _name = name;
+      _phone = phone;
+      _address = address;
+      _id=_contacts.Count;
+    }
+    //Add my getters and setters
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public void SetPhone(string newPhone)
+    {
+      _phone = newPhone;
+    }
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(string newAddress)
+    {
+      _address=newAddress;
+    }
+    public static List<Contact> GetAll()
+    {
+      return _contacts;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+
+    public static Contact Find(int searchId)
+    {
+      return _contacts[searchId-1];
+    }
+    // public static void ClearAll()
+    // {
+    //   _id=1;
+    //   _contacts.Clear();
+    // }
+  }
+}
